b009e42865997076d158275034bbb410c8f5486b
dhruw <dhruwlalan22@gmail.com>
2020-09-21 11:19:05 +0530

    Initial Commit

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+// ASSETS
+import './assets.js';
+
+// MAIN
---
+++ webpack.start.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	mode: 'development' ,
+	devtool: 'source-map' ,
+	entry: { index: './src/js/index.js' } ,
+	output: {
+		filename: '[name].bundle.js' ,
+		path: path.resolve(__dirname, 'dist') ,
+	} ,
+	module: {
+		rules: [
+			{
+				test: /\.html$/ ,
+				use: ['html-loader'] ,
+			} ,
+			{
+				test: /\.css$/ ,
+				use: [
+					'style-loader' ,
+					{ loader: 'css-loader' , options: { url: false, } } ,
+				] ,
+			} ,
+			{
+				test: /\.scss$/ , 
+				use: [
+					'style-loader' ,
+					{ loader: 'css-loader' , options: { url: false, } } ,
+					'sass-loader' ,
+				] ,
+			} ,
+			{
+				test: /\.js$/ ,
+				exclude: /node_modules/ ,
+				use: ['babel-loader'] ,
+			} ,
+			{
+				test: /\.ico$/ ,
+				use: {
+					loader: 'file-loader' ,
+					options: { name: 'favicon.ico' , outputPath: 'assets/favicon'} ,
+				} ,
+			} ,
+			{
+				test: /\.svg$/ ,
+				use: {
+					loader: 'file-loader' ,
+					options: { name: '[name].[ext]' , esModule: false , outputPath: 'assets/svg' } ,
+				} ,
+			} ,
+			{
+				test: /\.(jpeg|png|jpg|gif)$/ ,
+				use: {
+					loader: 'file-loader' ,
+					options: { name: '[name].[ext]' , esModule: false , outputPath: 'assets/images' } ,
+				} ,
+			} ,
+		] ,
+	} ,
+	plugins: [
+		new HtmlWebpackPlugin({ 
+			filename: 'index.html' ,
+			template: path.resolve(__dirname, 'src', 'index.html') ,
+			chunks: ['index'] ,
+		}) ,
+	] ,
+	optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/ ,
+                    name: 'vendor' ,
+                    chunks: 'all' ,
+                    enforce: true ,
+                }
+            }
+        }
+    } ,
+};
